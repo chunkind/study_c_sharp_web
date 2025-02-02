960f215adc7e2e3f124d3426091cc94d9841ce7d
chunkind <chunkind@naver.com>
2025-02-02 10:04:13 +0900

    2/2 :: HelloWebAPI

---
+++ HelloEmpty/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using HelloEmpty.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloEmpty.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public List<HelloMessage> Get()
+        {
+            List<HelloMessage> message = new List<HelloMessage>();
+            message.Add(new HelloMessage { Message = "Hello Web API 1 !" });
+            message.Add(new HelloMessage { Message = "Hello Web API 2 !" });
+            message.Add(new HelloMessage { Message = "Hello Web API 3 !" });
+
+            return message;
+        }
+    }
+}
