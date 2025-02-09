b2bf27efbf3366d64f59c1f03cd31b8dfe9b7b67
chunkind <chunkind@naver.com>
2025-02-09 23:18:48 +0900

    2/9 :: WebApi

---
+++ WebApi/Controllers/RankingController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedData.Models;
+using WebApi.Data;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankingController : ControllerBase
+    {
+        ApplicationDbContext _context;
+
+        public RankingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create
+        [HttpPost]
+        public GameResult AddGameResult([FromBody] GameResult gameResult)
+        {
+            _context.GameResult.Add(gameResult);
+            _context.SaveChanges();
+
+            return gameResult;
+        }
+
+        // Read
+        [HttpGet]
+        public List<GameResult> GetGameResult()
+        {
+            List<GameResult> results = _context.GameResult
+                .OrderByDescending(item => item.Score)
+                .ToList();
+
+            return results;
+        }
+
+        [HttpGet("{id}")]
+        public GameResult GetGameResult(int id)
+        {
+            GameResult result = _context.GameResult
+                .Where(item => item.Id == id)
+                .FirstOrDefault();
+
+            return result;
+        }
+
+        // Update
+        [HttpPut]
+        public bool UpdateGameResult([FromBody] GameResult gameResult)
+        {
+            var findResult = _context.GameResult
+                .Where(x => x.Id == gameResult.Id)
+                .FirstOrDefault();
+
+            if (findResult == null)
+                return false;
+
+            findResult.UserName = gameResult.UserName;
+            findResult.Score = gameResult.Score;
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        // Delete
+        [HttpDelete("{id}")]
+        public bool DeleteGameResult(int id)
+        {
+            var findResult = _context.GameResult
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            if(findResult == null)
+                return false;
+
+            _context.GameResult.Remove(findResult);
+            _context.SaveChanges();
+
+            return true;
+        }
+    }
+}
---
+++ WebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedData.Models;
+
+namespace WebApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<GameResult> GameResult {  get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
