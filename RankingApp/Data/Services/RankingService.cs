bcb4f441ced6947f41968a3df6784bcbe367c29d
chunkind <chunkind@naver.com>
2025-02-09 21:05:54 +0900

    2/9 :: 앱만들기 4

--- RankingApp/Data/Services/RankingService.cs
+++ RankingApp/Data/Services/RankingService.cs
@@ -24,11 +24,44 @@ namespace RankingApp.Data.Services
         // Read
         public Task<List<GameResult>> GetGameResultsAsync()
         {
-            List<GameResult>  results = _context.GameResults
+            List<GameResult> results = _context.GameResults
                 .OrderByDescending(item => item.Score)
                 .ToList();
 
             return Task.FromResult(results);
+        }
+
+        // Update
+        public Task<bool> UpdateGameResult(GameResult gameResult)
+        {
+            var findResult = _context.GameResults
+                .Where(x => x.Id == gameResult.Id)
+                .FirstOrDefault();
+
+            if (findResult == null)
+                return Task.FromResult(false);
+
+            findResult.UserName = gameResult.UserName;
+            findResult.Score = gameResult.Score;
+            _context.SaveChanges();
+
+            return Task.FromResult(true);
+        }
+
+        // Delete
+        public Task<bool> DeleteGameResult(GameResult gameResult)
+        {
+            var findResult = _context.GameResults
+                .Where(x => x.Id == gameResult.Id)
+                .FirstOrDefault();
+
+            if(findResult == null)
+                return Task.FromResult(false);
+
+            _context.GameResults.Remove(gameResult);
+            _context.SaveChanges();
+
+            return Task.FromResult(true);
         }
     }
 }
