381eb6439cf582a6ee9b3bd5ae5220976a69c394
chunkind <chunkind@naver.com>
2025-02-09 23:29:02 +0900

    2/9 :: 변경사항 없음

--- RankingApp/Data/Models/GameResult.cs
+++ RankingApp/Data/Models/GameResult.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public string UserName {  get; set; }
+        public string UserName { get; set; }
         public int Score { get; set; }
-        public DateTime Date {  get; set; }
+        public DateTime Date { get; set; }
     }
 }
