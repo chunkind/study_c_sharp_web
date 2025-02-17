ca00a8a6deeb762778f1e8a56f83d3e82ef22d73
chunkind <chunkind@naver.com>
2025-02-17 23:13:47 +0900

    2/17 ::

---
+++ N03_Relationship/DbCommands.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using MMO_EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N02_CRUD
+{
+    public class DbCommands
+    {
+        public static void InitializeDB(bool forceReset = false)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                if (!forceReset && (db.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists())
+                    return;
+
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                CreateTestData(db);
+                Console.WriteLine("DB Initialized");
+            }
+
+            // using 쓰면 안해도됨
+            //db.Dispose();
+        }
+
+        public static void CreateTestData(AppDbContext db)
+        {
+            var player = new Player()
+            {
+                Name = "chunkind"
+            };
+
+            List<Item> items = new List<Item>()
+            {
+                new Item()
+                {
+                    TemplateId = 101,
+                    CreateDate = DateTime.Now,
+                    Owner = player
+                },
+                new Item()
+                {
+                    TemplateId = 102,
+                    CreateDate = DateTime.Now,
+                    Owner = player
+                },
+                new Item()
+                {
+                    TemplateId = 103,
+                    CreateDate = DateTime.Now,
+                    Owner = new Player() { Name = "Faker" }
+                }
+            };
+
+            db.items.AddRange(items);
+            db.SaveChanges();
+        }
+
+        public static void ReadAll()
+        {
+            using (var db = new AppDbContext())
+            {
+                // AsNoTracking : ReadOnly << Tracking Snapshot 이라고 데이터 변경 탐지하는 기능 때문
+                foreach(Item item in db.Items.AsNoTracking().Include(i=>i.Owner))
+                {
+                    Console.WriteLine($"TemplateId({item.TemplateId}) Owner({item.Owner.Name}) Created({item.CreateDate})");
+                }
+            }
+        }
+
+        public static void ShowItems()
+        {
+            Console.WriteLine("플레이어 이름 입력하세요.");
+            Console.Write("> ");
+            string name = Console.ReadLine();
+
+            //using (var db = new AppDbContext())
+            //{
+            //    foreach(Player player in db.Players.AsNoTracking().Where(p=>p.Name == name).Include(p=>p.Items))
+            //    {
+            //        // 1:N 관계여도 자동으로 로딩되는게 아니다. => Include 를 해줘야 함
+            //        foreach(Item item in player.Items)
+            //        {
+            //            Console.WriteLine($"{item.TemplateId}");
+            //        }
+            //    }
+            //}
+
+        }
+    }
+}
---
+++ N03_Relationship/Program.cs
@@ -0,0 +1,41 @@
+﻿using N02_CRUD;
+using System;
+
+namespace MMO_EFCore
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            DbCommands.InitializeDB(forceReset: false);
+
+            // CRUD
+            Console.WriteLine("명령어를 입력하세요");
+            Console.WriteLine("[0] ForceReset");
+            Console.WriteLine("[1] ReadAll");
+            Console.WriteLine("[2] ShowItems");
+
+            while(true)
+            {
+                Console.WriteLine("> ");
+                string command = Console.ReadLine();
+                switch(command)
+                {
+                    case "0":
+                        DbCommands.InitializeDB(forceReset: true);
+                        break;
+                    case "1":
+                        DbCommands.ReadAll();
+                        break;
+                    case "2":
+                        DbCommands.ShowItems();
+                        break;
+                    case "3":
+                        break;
+                }
+            }
+        }
+    }
+}
